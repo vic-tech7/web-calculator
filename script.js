084d6441cb92791f3ff9ff22a8e7847c008f1064
vic-tech7 <chadhackerhat@gmail.com>
2026-05-16 08:57:06 -0400

    new fixed veccalculator

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ function sendAIQuery() {
     })
     .catch(error => {
       messages.innerHTML += `
-        <div><b>VEC AI:</b> Connection error. Check API deployment.</div>
+        <div><b>VEC AI:</b> Connection error. Check API deploymented.</div>
       `;
       console.error(error);
     });
